15274a4847dd32eee898e3eab5de318d1c692c85
Expedito Andrade <expeditojazz@gmail.com>
2020-08-04 14:29:58 -0300

    functions are now modular

---
+++ src/components/genLineLider.js
@@ -0,0 +1,56 @@
+const genLiderLine = (list, lider, month, fbSeguidores ) => {
+
+  const { tempLider, tempMes, tempFbSeguidores } = arrGenSearch(list, lider,  month, fbSeguidores );
+
+  const chart = new Chart('chart', {
+    type: 'line',
+  
+    data: {
+      labels: tempMes,
+      datasets: [{
+        label: list[0],
+        data: tempFbSeguidores,
+        borderColor: "#3e95cd"
+      }, 
+  
+    ]
+    },
+    options: {showLines: true}
+  })
+  return chart;
+}
+
+const arrGenBase = (lideres) => {
+  const listLideres = lideres.map((arr) => {
+    return arr.profile
+  })
+  const month = lideres.map((arr) => {
+    return arr.month
+  })
+  const fbSeguidores = lideres.map(arr => arr.fb_number_likes);
+  return {
+    listLideres: listLideres,
+    month: month,
+    fbSeguidores: fbSeguidores
+  }
+}
+
+const arrGenSearch = (list, queryLider, month, fbSeguidores ) => {
+  console.log(list)
+  const obj = {
+    tempLider: [],
+    tempMes: [],
+    tempFbSeguidores: []
+  };
+  list.forEach((lider, index) => {
+    if (lider === queryLider) {
+      obj.tempLider.push(lider);
+      obj.tempMes.push(month[index]);
+      obj.tempFbSeguidores.push(fbSeguidores[index]);
+    } 
+  })
+  console.log(obj)
+  return obj
+}
+
+export default { genLiderLine, arrGenSearch, arrGenBase }
--- src/index.js
+++ src/index.js
@@ -1,63 +1,34 @@
 import * as d3 from 'd3'
 import Chart from 'chart.js';
-import populateSelect from './components/SelectMenu'
+import populateSelect from './components/SelectMenu';
+import lineGraph from './components/genLineLider'
 
 const makeChart = (lideres) => {
+
+
+  const { genLiderLine, arrGenSearch, arrGenBase} = lineGraph;
   populateSelect.populateSelect();
-  console.log(lideres);
   const selects = document.getElementById('line');
+  const {listLideres, month, fbSeguidores} = arrGenBase(lideres);
 
 
-  const listLideres = lideres.map((arr) => {
-    return arr.profile
-  })
-  const month = lideres.map((arr) => {
-    return arr.month
-  })
-  const fbSeguidores = lideres.map(arr => arr.fb_number_likes)
-  // console.log(fbSeguidores)
-  // console.log(month)
-  // console.log(listLideres);
-  let tempLider = [];
-  let tempMes = [];
-  let tempFbSeguidores = [];
-  let selectResult = listLideres[0];
-  listLideres.forEach((lider, index) => {
-    if (lider === selectResult) {
-      tempLider.push(lider);
-      tempMes.push(month[index]);
-      tempFbSeguidores.push(fbSeguidores[index]);
-    } 
-  })
 
-  const chart = new Chart('chart', {
-    type: 'line',
+  const chart = genLiderLine(listLideres, listLideres[0], month, fbSeguidores );
 
-    data: {
-      labels: tempMes,
-      datasets: [{
-        label: listLideres[0],
-        data: tempFbSeguidores,
-        borderColor: "#3e95cd"
-      }, 
- 
-    ]
-    },
-    options: {showLines: true}
-  })
   selects.addEventListener('change', (e) => {
-    tempLider = [];
-    tempMes = [];
-    tempFbSeguidores = [];
+
     const selectResult = e.target.value;
-    listLideres.forEach((lider, index) => {
-      if (lider === selectResult) {
-        tempLider.push(lider);
-        tempMes.push(month[index]);
-        tempFbSeguidores.push(fbSeguidores[index]);
-      } 
-    })
-  
+    // listLideres.forEach((lider, index) => {
+    //   if (lider === selectResult) {
+    //     tempLider.push(lider);
+    //     tempMes.push(month[index]);
+    //     tempFbSeguidores.push(fbSeguidores[index]);
+    //   }
+    // })
+
+    const { tempLider, tempMes, tempFbSeguidores } = arrGenSearch(listLideres, selectResult ,  month, fbSeguidores );
+
+
     console.log(e.target.value);
     chart.data.datasets.forEach((dataset) => {
       dataset.label = tempLider[0];
@@ -74,27 +45,27 @@ d3.csv('./src/dados.csv').then(makeChart)
 //   type: 'line',
 //   data: {
 //     labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-//     datasets: [{ 
+//     datasets: [{
 //         data: [86,114,106,106,107,111,133,221,783,2478],
 //         label: "Africa",
 //         borderColor: "#3e95cd",
 //         fill: false
-//       }, { 
+//       }, {
 //         data: [282,350,411,502,635,809,947,1402,3700,5267],
 //         label: "Asia",
 //         borderColor: "#8e5ea2",
 //         fill: false
-//       }, { 
+//       }, {
 //         data: [168,170,178,190,203,276,408,547,675,734],
 //         label: "Europe",
 //         borderColor: "#3cba9f",
 //         fill: false
-//       }, { 
+//       }, {
 //         data: [40,20,10,16,24,38,74,167,508,784],
 //         label: "Latin America",
 //         borderColor: "#e8c3b9",
 //         fill: false
-//       }, { 
+//       }, {
 //         data: [6,3,2,2,7,26,82,172,312,433],
 //         label: "North America",
 //         borderColor: "#c45850",
@@ -109,3 +80,4 @@ d3.csv('./src/dados.csv').then(makeChart)
 //     }
 //   }
 // });
+
